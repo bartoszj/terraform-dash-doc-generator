d460e52fa0067d25cd8f49e9286bbf2055deb85b
Bartosz Janda <bartosz.janda@gmail.com>
2020-12-06 14:24:05 +0100

    Fixed layout handling

--- git_get.sh
+++ git_get.sh
@@ -132,13 +132,15 @@ for i in ${!provider_names[@]}; do
 done
 
 # Fixes
-## Remove `layout:` from AWS, Scaffolding, GitLab docs
+## Remove `layout:` from some providers
 if [[ ${OSTYPE} == "linux-gnu"* ]]; then
   find ${TERRAFORM_PATH}/ext/providers/aws/website/docs \( -name "*.markdown" -or -name "*.md" \) -exec sed -e "/layout:/d" -i"" {} \;
   find ${TERRAFORM_PATH}/ext/providers/gitlab/docs \( -name "*.markdown" -or -name "*.md" \) -exec sed -e "/layout:/d" -i"" {} \;
+  find ${TERRAFORM_PATH}/ext/providers/rancher2/docs \( -name "*.markdown" -or -name "*.md" \) -exec sed -e "/layout:/d" -i"" {} \;
   find ${TERRAFORM_PATH}/ext/providers/scaffolding/website/docs \( -name "*.markdown" -or -name "*.md" \) -exec sed -e "/layout:/d" -i"" {} \;
 else
   find ${TERRAFORM_PATH}/ext/providers/aws/website/docs \( -name "*.markdown" -or -name "*.md" \) -exec sed -e "/layout:/d" -i "" {} \;
   find ${TERRAFORM_PATH}/ext/providers/gitlab/docs \( -name "*.markdown" -or -name "*.md" \) -exec sed -e "/layout:/d" -i "" {} \;
+  find ${TERRAFORM_PATH}/ext/providers/rancher2/docs \( -name "*.markdown" -or -name "*.md" \) -exec sed -e "/layout:/d" -i "" {} \;
   find ${TERRAFORM_PATH}/ext/providers/scaffolding/website/docs \( -name "*.markdown" -or -name "*.md" \) -exec sed -e "/layout:/d" -i "" {} \;
 fi
